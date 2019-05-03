e7c4df3435ee5931ff1b53e480cfb4a7a01d8629
Austin Abell <austinabell8@gmail.com>
2019-05-03 13:27:12 -0400

    Fixed bug and updated path to go-ethereum

--- go/main.go
+++ go/main.go
@@ -5,7 +5,7 @@ import (
 	"math/big"
 
 	"github.com/ETCDEVTeam/sputnikvm-ffi/go/sputnikvm"
-	"github.com/ethereumclassic/go-ethereum/common"
+	"github.com/eth-classic/go-ethereum/common"
 )
 
 func main() {
--- go/sputnikvm/sputnikvm.go
+++ go/sputnikvm/sputnikvm.go
@@ -36,7 +36,7 @@ import (
 	"math/big"
 	"unsafe"
 
-	"github.com/ethereumclassic/go-ethereum/common"
+	"github.com/eth-classic/go-ethereum/common"
 )
 
 type AccountChangeType int
